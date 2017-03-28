4d741db3a5b1f36f1172b930896b94464568490a
s-122-20 <s-122-20@gumiforest.ae.krakow.pl>
2017-03-28 12:02:59 +0200

    added interests

---
+++ www/js/pgb.js
@@ -0,0 +1,35 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(2);
+	Info();
+}
+
+function info() {
+
+	name = 'Adam';
+	surname = 'Malysz';
+	email = 'adammalysz@onet.pl';
+	studyfield = 'Informatyka stosowana';
+	
+	info =  'Name   		: '    + name + '<br>' + 
+			'Surname	    : '     + surname + '<br>' + 
+			'Email		 	: '  + email + '<br>' + 
+			'Field of Study	: ' + studyfield + '<br>';
+
+	document.getElementById("userDetails").innerHTML = info;	
+}
+
+function iterests() {
+
+	name = 'Adam';
+	surname = 'Malysz';
+	email = 'adammalysz@onet.pl';
+	studyfield = 'Informatyka stosowana';
+	
+	interests =  'Moje zainteresowania: W wolnym czasie czytam moją ulubioną literaturę. Uwielbiam kryminały, książki przygodowe, fantastyczne, dramaty. Zazwyczaj mocno angażuję się w przeżycia głównych bohaterów. Literatura pochłania wiele mojego czasu, bo po przeczytaniu kilku kartek jestem ciekawa co będzie dalej, jak ta historia się skończy i dlatego zdarza się, że jednego dnia czytam całą książkę.';
+
+	document.getElementById("interestsDetails").innerHTML = interests;	
+}
